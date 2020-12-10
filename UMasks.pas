9b92b82c2b84393fe31ed21e8c3329826ca92561
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2020-12-10 19:13:39 -0300

    trim masks lines and bypass empty lines

--- UMasks.pas
+++ UMasks.pas
@@ -16,13 +16,18 @@ class function TMasks.GetMasks(const DefinitionMasks: string): string;
 var
   S: TStringList;
   I: Integer;
+  A: string;
 begin
   S := TStringList.Create;
   try
     S.Text := DefinitionMasks;
 
     for I := S.Count-1 downto 0 do
-      if S[I].StartsWith('//') then S.Delete(I);
+    begin
+      A := Trim(S[I]);
+
+      if A.IsEmpty or A.StartsWith('//') then S.Delete(I);
+    end;
 
     Result := S.Text;
   finally
